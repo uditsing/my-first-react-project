521a3451e3e191bb069881769bc2998736527f6c
Uditsing <31947595+uditsing@users.noreply.github.com>
2020-08-03 02:22:10 +0530

    Updated

---
+++ precache-manifest.cce89613f1e55dc0fabc4c5ba74bb91d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a6d5e4c6c44e9b95965ddb8a56439e7",
+    "url": "/uditsing/my-first-react-project/index.html"
+  },
+  {
+    "revision": "dda5f632c9c7917a610d",
+    "url": "/uditsing/my-first-react-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/uditsing/my-first-react-project/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uditsing/my-first-react-project/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda5f632c9c7917a610d",
+    "url": "/uditsing/my-first-react-project/static/js/main.fde83c72.chunk.js"
+  },
+  {
+    "revision": "13b54cd901293581ef5e",
+    "url": "/uditsing/my-first-react-project/static/js/runtime-main.6d354459.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uditsing/my-first-react-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4a89a7ae9b941166d6d519c94ebf9db.js"
+  "/uditsing/my-first-react-project/precache-manifest.cce89613f1e55dc0fabc4c5ba74bb91d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uditsing/my-first-react-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
